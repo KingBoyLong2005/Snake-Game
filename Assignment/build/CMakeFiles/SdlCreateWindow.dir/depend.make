24f94e401227d708427c95d50c8890dfc0087b86
Long <nghailong2005@gmail.com>
2024-10-23 17:14:59 +0700

    done

--- Assignment/build/CMakeFiles/SdlCreateWindow.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SdlCreateWindow.
-# This may be replaced when dependencies are built.
--- Assignment/build/CMakeFiles/SdlCreateWindow.dir/src/Point.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/SdlCreateWindow.dir/src/Point.cpp.obj: \
- D:\DEV\ Game\Assignment\src\Point.cpp \
- D:/DEV\ Game/Assignment/include/Point.hpp
